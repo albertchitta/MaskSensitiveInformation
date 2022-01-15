3cacb52e88d1d848fda7ba8d054bfee7669f8645
albertchitta <albert.w.chittaphong@gmail.com>
2022-01-15 13:24:22 -0600

    Add project files.

---
+++ MaskSensitiveInformation/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Enter a secret: ");
+string input = Console.ReadLine();
+char[] array = input.ToCharArray();
+
+for (int i = 0; i < array.Length; i++)
+{
+    if (i < array.Length - 4)
+    {
+        Console.Write('*');
+    }
+    else
+    {
+        Console.Write(array[i]);
+    }
+}
